f61ee9fcf42afc457decf552b9bdb226990932e1
René Ruts <rene.ruts@student.howest.be>
2025-11-16 15:42:49 +0100

    added room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    val roomVersion = "2.8.3"
+
+    implementation("androidx.room:room-runtime:$roomVersion")
 }
