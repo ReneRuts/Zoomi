d1290d6be7629d1248b5c411182cb6dff0ba51c5
René Ruts <rene.ruts@student.howest.be>
2025-11-23 17:36:20 +0100

    Added userRepository

--- app/src/main/java/com/group1/zoomi/data/UserRepository.kt
+++ app/src/main/java/com/group1/zoomi/data/UserRepository.kt
@@ -1,4 +1,13 @@
 package com.group1.zoomi.data
 
-class UserRepository {
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository(private val userDao: UserDao) {
+
+    suspend fun registerUser(user: User) = userDao.insert(user)
+    suspend fun updateUser(user: User) = userDao.update(user)
+    suspend fun deleteUser(user: User) = userDao.delete(user)
+
+    fun getUser(id: Int): Flow<User> = userDao.getUser(id)
+    fun getAllUsers(): Flow<List<User>> = userDao.getAllUsers()
 }
