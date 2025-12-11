36a5ddb31a19913f292f852fa85e76066ff38470
gerbe <gerbe.arys@student.howest.be>
2025-12-11 16:43:42 +0100

    Commit before merge request

--- app/src/main/java/com/group1/zoomi/navigation/AppNavigation.kt
+++ app/src/main/java/com/group1/zoomi/navigation/AppNavigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.group1.zoomi.ui.screens.LoginScreen
+import com.group1.zoomi.ui.`login-screen`.LoginScreen
 import com.group1.zoomi.ui.home.OverviewScreen
 import com.group1.zoomi.ui.workout.WorkoutEntryScreen
 import com.group1.zoomi.ui.detail.DetailsScreen
