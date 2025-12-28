b7bb73d19ced8b0b8bc8b7f8e933ec68b6dbaea7
gerbe <gerbe.arys@student.howest.be>
2025-12-28 19:43:24 +0100

    Commit berfore pulling

--- app/src/main/java/com/group1/zoomi/ui/login/LoginScreen.kt
+++ app/src/main/java/com/group1/zoomi/ui/login/LoginScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -21,13 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.group1.zoomi.ui.theme.Green
-import com.group1.zoomi.ui.theme.Orange
 import com.group1.zoomi.ui.theme.ZoomiTheme
 import com.scottyab.rootbeer.RootBeer
 
